133366b4d4b921a71e8b4cceb9ff58269bc66a1c
Sulaim-Saadath <avoor2005@gmail.com>
2026-08-07 12:14:36 +0530

    Sigin/Signup

--- feastoApp/urls.py
+++ feastoApp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.say_hello),
+    path('', views.say_hello, name="say_hello"),
+    path('open_signup', views.open_signup, name="open_signup"),
+    path('open_signin', views.open_signin, name="open_signin"),
+    path('signup', views.signup, name="signup"),
 ]
--- feastoApp/views.py
+++ feastoApp/views.py
@@ -5,3 +5,20 @@ from django.shortcuts import render
 def say_hello(request):
     # return HttpResponse("Say hello my app is working!")
     return render(request, "index.html")
+
+def open_signup(request):
+    return render(request, "signup.html")
+
+def open_signin(request):
+    return render(request, "signin.html")
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        mobile = request.POST.get('mobile')
+        address = request.POST.get('address')
+        return HttpResponse(f"{username}, {password}, {email}, {mobile}, {address}")
+    else:
+        return HttpResponse("Invalid Response!")
