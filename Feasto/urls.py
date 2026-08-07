25b0daa2bfc653dfa08564202943b68e2537d0cd
Sulaim-Saadath <avoor2005@gmail.com>
2026-08-07 09:58:08 +0530

    Initial Commit

--- Feasto/urls.py
+++ Feasto/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("feastoApp.urls")),
+    path("", include("feastoApp.urls")), # Linking to feastoApp
 ]
--- feastoApp/views.py
+++ feastoApp/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def say_hello(request):
+    # return HttpResponse("Say hello my app is working!")
+    return render(request, "index.html")
